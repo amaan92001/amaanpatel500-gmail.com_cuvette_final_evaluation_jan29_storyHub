bee56418b1d37cea3df4575b7ca3a3f7681256e8
amaan92001 <113185353+amaan92001@users.noreply.github.com>
2024-09-29 14:07:28 +0530

    Project Done

---
+++ frontend/src/constants/data.js
@@ -0,0 +1,34 @@
+const filters = [
+  {
+    name: "All",
+    imageUrl:
+      "https://imgs.search.brave.com/l6xx9QBG8xP7DMTWyJlS_vIVIg4mVKKnuWYtRwey9AU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTU3/NDI0MjI4L3Bob3Rv/L2V0aGVyaWFsLWlt/YWdlLW9mLXRoZS1o/ZWF2ZW5zLmpwZz9z/PTYxMng2MTImdz0w/Jms9MjAmYz16ZW0z/b3F1ejFhUk1TYmc3/c0Y3MmljV3NvTFpW/SGdiMXR5R0tSZlBT/Nl9NPQ",
+  },
+  {
+    name: "Food",
+    imageUrl:
+      "https://imgs.search.brave.com/sDn47VHb6YY42wY6vU-Hm03w4XYRFflLpXc8_fOMf_o/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxNy8x/MC8wOS8xOS8yOS9l/YXQtMjgzNDU0OV82/NDAuanBn",
+  },
+  {
+    name: "Fitness",
+    imageUrl:
+      "https://imgs.search.brave.com/Qdo-oLBdiYgDhtjhvpmZaJu7Rm7DfIq8WOE7uxayobE/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/ZnJlZS1waG90by9m/dWxsLWJvZHktcG9y/dHJhaXQtYXRobGV0/aWMtc2hpcnRsZXNz/LW1hbGUtZG9pbmct/YmljZXBzLXdvcmtv/dXRzLXdpdGgtZHVt/YmJlbGxzLWd5bS1j/bHViXzYxMzkxMC0x/MTc2LmpwZz9zaXpl/PTYyNiZleHQ9anBn",
+  },
+  {
+    name: "Travel",
+    imageUrl:
+      "https://imgs.search.brave.com/2H2f1LvWf9i_CrfCkkdn-RrDzzu7o8T14N4_YsOfYqY/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTQ1/MDA1MjQ5NS9waG90/by9oYXBweS1mYW1p/bHktd2l0aC1jYXIt/dHJhdmVsLXJvYWQt/dHJpcC1zdW1tZXIt/dmFjYXRpb24taW4t/Y2FyLWluLXRoZS1z/dW5zZXQtZGFkLW1v/bS1hbmQuanBnP3M9/NjEyeDYxMiZ3PTAm/az0yMCZjPXlqNHRz/WHZmb2ZkTkM5eWFX/b3RRckowQ0pQZU1R/b1UyemZmWEZzRENP/eE09",
+    },
+  {
+    name: "Movie",
+    imageUrl:
+      "https://imgs.search.brave.com/EjZ2xR5ASmALj59uUhozWzjtcPIXh27l05XktsQCa_c/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly93d3cu/Y2FydG9vbmJ1Y2tl/dC5jb20vd3AtY29u/dGVudC91cGxvYWRz/LzIwMTYvMDcvQmF0/bWFuLVBpYy02MDB4/Mzc1LmpwZw",
+  },
+  {
+    name: "Education",
+    imageUrl:
+      "https://imgs.search.brave.com/e92y2WguoWqNbp3vtHckUhDIvENAVof4sLWNB3f1p10/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/Y3JlYXRlLnZpc3Rh/LmNvbS9hcGkvbWVk/aWEvc21hbGwvMTkw/Nzg3NTI0L3N0b2Nr/LXBob3RvLWVkdWNh/dGlvbg.jpeg",
+  },
+];
+
+export default filters;
